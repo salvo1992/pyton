a0785159a3fcb9e82ea9ac42b459d6c021393268
salvo1992 <145794449+salvo1992@users.noreply.github.com>
2024-11-13 19:15:26 +0100

    ...

--- python-couse-epicode.py
+++ python-couse-epicode.py
@@ -214,29 +214,29 @@
 # dalla lista.
 
 
-items = ["fagioli", "noci", "mele"]
-items.sort()
+# items = ["fagioli", "noci", "mele"]
+# items.sort()
 
 
-for item in items:
-print("-", item)
+# for item in items:
+#  print("-", item)
 
 
-user_item = input("Aggiungi un articolo: ").lower().strip()
+# user_item = input("Aggiungi un articolo: ").lower().strip()
 
 
-if not user_item:
-print("Errore: Non hai inserito alcun articolo")
-elif user_item in items:
-print("Errore: Articolo già presente")
-else:
-items.append(user_item)
-print("Articolo aggiunto ✅")
-items.sort()
+# if not user_item:
+#  print("Errore: Non hai inserito alcun articolo")
+# elif user_item in items:
+#  print("Errore: Articolo già presente")
+# else:
+#  items.append(user_item)
+# print("Articolo aggiunto ✅")
+# items.sort()
 
 
-for item in items:
-print("-", item)
+# for item in items:
+#  print("-", item)
 
 
 
@@ -249,3 +249,228 @@ print("-", item)
 # i quali vengono poi aggiunti alla lista iniziale.
 # Per semplicità, non controllare se l'utente
 # inserisce articoli già presenti o stringhe vuote.
+
+
+# items = ["FAGIOLI", "NOCI", "MELE"]
+# split_char = "|"
+# items.sort()
+
+
+# for item in items:
+#  print("-", item)
+
+
+# user_item = input("Aggiungi un articolo: ").upper().strip()
+
+
+# if split_char in user_item:
+#  to_add_items = user_item.split(split_char)
+#  items.extend(to_add_items)
+#  print("Articoli aggiunti")
+#  items.sort()
+# elif not user_item:
+#  print("Errore: Non hai inserito alcun articolo")
+# elif user_item in items:
+#  print("Errore: Articolo già presente")
+#  user_answer = input("Vuoi rimuoverlo? In caso affermativo scrivi si: ")
+# if user_answer == "si":
+#  items.remove(user_item)
+#  print("Articolo rimosso")
+# else:
+#  print("Articolo non rimosso")
+# else:
+# items.append(user_item)
+#  print("Articolo aggiunto ✅")
+#  items.sort() 
+
+
+#  for item in items:
+#   print("-", item)
+
+
+
+# panda = {
+# "color": "red",
+# "price": 35_000.0,
+# "year": 2024,
+# "hybrid": True
+# }
+
+
+# punto = {
+# "color": "green",
+# "price": 25_000.0,
+# "year": 2014,
+# "hybrid": False
+# }
+
+
+
+# def get_discounted_price(price):
+#  discounted_price = price * 0.7
+#  if discounted_price > 20_000:
+#   discounted_price -= 100
+#  print(discounted_price)
+
+
+
+# get_discounted_price(panda["price"])
+
+
+
+# panda = {
+# "color": "red",
+# "price": 35_000.0,
+# "year": 2024,
+# "hybrid": True
+# }
+
+
+# punto = {
+# "color": "green",
+# "price": 25_000.0,
+# "year": 2014,
+# "hybrid": False
+# }
+
+
+
+# def get_discounted_price(price, *bonus_discounts):
+# discounted_price = price * 0.7
+# if discounted_price > 20_000:
+# for discount in bonus_discounts:
+# discounted_price -= discount
+# return discounted_price
+
+
+
+# print(get_discounted_price(panda["price"], 100, 150, 200))
+
+ 
+# Modifica l'esercizio della lezione precedente,
+# (vedi codice di partenza in chat)
+# ma basandolo su un dizionario, invece che su una lista.
+# Il dizionario iniziale deve contenere
+# come chiave il prodotto da acquistare
+# e come valore la sua quantità.
+# All'utente sarà richiesto di inserire
+# sia il prodotto che la quantità.
+# Non è necessario ordinare il dizionario.
+
+
+# BONUS:
+# Se l'articolo è già presente,
+# chiedi di inserire la quantità
+# e aggiornala sul dizionario.
+
+
+# converti items da lista in dizionario {key:val, ...}
+# items = {
+# "fagioli": 10,
+# "noci": 5,
+# "mele": 15
+# }
+
+
+# # stampa le coppie chiave e valore del dizionario items.items()
+# for item, quantity in items.items():
+#  print(f"- {item} ({quantity} pezzi)")
+
+
+# user_item = input("Aggiungi un articolo: ").lower().strip()
+
+
+# if not user_item:
+#  print("Errore: Non hai inserito alcun articolo")
+# elif user_item in items:
+#    print("Articolo già presente")
+# # richiedi all'utente la quantità
+#    user_quantity = int(input("Inserisci la quantità: ").strip())
+# # sul dizionario modifica il valore della chiave già presente items[new_key] = new_val
+#    items[user_item] = user_quantity
+#    print("Articolo aggiornato ✅")
+# else:
+# # richiedi all'utente la quantità
+#   user_quantity = int(input("Inserisci la quantità: ").strip())
+# # sul dizionario crea la nuova coppia chiave valore items[new_key] = new_val
+#   items[user_item] = user_quantity
+# print("Articolo aggiunto ✅")
+
+
+# # vedi ciclo precedente
+# for item, quantity in items.items():
+#   print(f"- {item} ({quantity} pezzi)")
+    
+
+
+person1={
+  "name": "John",
+  "surname": "Doe",
+  "age": None,
+  "city": "New York",
+  "hobbies": ["reading", "painting"]
+}
+
+person2={    
+  "name": "Jane",
+  "surname": "Doe",
+  "age": None,
+  "city": "New York",
+  "hobbies": ["painting", "hiking"]
+}
+def calculate_age(name, current_year ):
+   birth_year = int(input(f" {name}, inserisci il tuo anno di nascita: "))
+   age = current_year - birth_year
+   return age
+
+
+person1["age"] = calculate_age(person1["name"],2024) 
+person2["age"] = calculate_age(person2["name"],2024) 
+
+
+print(person1)
+print(person2)
+
+# # Crea una classe Pokemon coi seguenti attributi:
+# Nome, Salute e Forza.
+# Implementa un metodo di attacco che permetta
+# a un Pokemon di attaccarne un altro,
+# riducendo la Salute dell'avversario.
+# (al metodo passeremo come argomento un oggetto Pokemon)
+# La Salute dell'avversario viene ridotta
+# di un valore pari alla Forza dell'attaccante.
+# Il metodo infine stampa un messaggio,
+# con alcuni dettagli sull'attacco, ad esempio:
+# Pikachu attacca Charizard causando 15 danni!
+
+
+# Completata la classe, istanzia due Pokemon
+# e falli attaccare l'un l'altro, quante volte desideri.
+# Dopo il combattimento, stampa la Salute
+# di entrambi, per vedere se è coerente con gli attacchi.
+
+
+
+
+ 
+class Pokemon:
+    def __init__(self, name, health, strength):
+        self.name = name
+        self.health = health
+        self.strength = strength
+
+    def attacks(self, enemy):
+        enemy.health -= self.strength
+        print(f"{self.name} attacca {enemy.name} causando {self.strength} danni!")
+
+pikachu = Pokemon("Pikachu", 100, 15)
+charizard = Pokemon("Charizard", 150, 20)
+
+pikachu.attacks(charizard)
+charizard.attacks(pikachu)
+pikachu.attacks(charizard)
+
+print(pikachu.health, charizard.health)
+
+
+
